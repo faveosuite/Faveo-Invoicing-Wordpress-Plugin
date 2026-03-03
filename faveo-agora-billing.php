561c68cf6a8f9a6f63326783c26c5eb6d1285ff0
shivapuranik <shivaleela.shivanand@ladybirdweb.com>
2026-03-03 16:37:06 +0530

    test domain updated

--- faveo-agora-billing.php
+++ faveo-agora-billing.php
@@ -9,25 +9,25 @@
  * Icon: /wp-content/plugins/faveo-agora-invoicing/assets/plugin-icon.png
  */
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 // Plugin directory paths
-define('FHAI_DIR', plugin_dir_path(__FILE__));
-define('FHAI_URL', plugin_dir_url(__FILE__));
+define( 'FHAI_DIR', plugin_dir_path( __FILE__ ) );
+define( 'FHAI_URL', plugin_dir_url( __FILE__ ) );
 
 // Include Files
 require_once FHAI_DIR . 'includes/admin-settings.php';
 require_once FHAI_DIR . 'includes/faveo-pricing-functions.php';
 
 // Styles and scripts
-add_action('wp_enqueue_scripts', 'fhai_data_enqueue_scripts');
+add_action( 'wp_enqueue_scripts', 'fhai_data_enqueue_scripts' );
 
 function fhai_data_enqueue_scripts() {
-    wp_enqueue_style('api-data-style', FHAI_URL . 'css/style.css', array(), '1.0'); 
-    wp_enqueue_script('api-data-script', FHAI_URL . 'js/faveo-pricing-script.js', array('jquery'), '1.0', true);
+	wp_enqueue_style( 'api-data-style', FHAI_URL . 'css/style.css', array(), '1.0' );
+	wp_enqueue_script( 'api-data-script', FHAI_URL . 'js/faveo-pricing-script.js', array( 'jquery' ), '1.0', true );
 }
 
 // Shortcode
-add_shortcode('fhai', 'fhai_calling');
+add_shortcode( 'fhai', 'fhai_calling' );
--- templates/products-template.php
+++ templates/products-template.php
@@ -1,6 +1,6 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly.
+	exit; // Exit if accessed directly.
 }
 /**
  * Products Template
@@ -14,173 +14,188 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 ?>
 
-<?php if ($all_status_one) : ?>
-    <center>
-        <div class="toggle-wrapper">
-            <div class="toggle-labels">
-                <span class="toggle-label monthly">Monthly</span>
-                <label class="toggle-switch">
-                    <input type="checkbox" class="pricing-toggle" data-group="<?php echo esc_attr($group_id); ?>">
-                    <span class="slider"></span>
-                </label>
-                <span class="toggle-label yearly">Yearly</span>
-            </div>
-        </div>
-    </center>
+<?php if ( $all_status_one ) : ?>
+	<center>
+		<div class="toggle-wrapper">
+			<div class="toggle-labels">
+				<span class="toggle-label monthly">Monthly</span>
+				<label class="toggle-switch">
+					<input type="checkbox" class="pricing-toggle" data-group="<?php echo esc_attr( $group_id ); ?>">
+					<span class="slider"></span>
+				</label>
+				<span class="toggle-label yearly">Yearly</span>
+			</div>
+		</div>
+	</center>
 <?php endif; ?>
 
 <div class="products-wrapper" data-count="0">
 
-<?php 
-$fhai_currency_code = $fhai_products_data['currency'] ?? '';
-$fhai_currency_symbol = fhai_currency_symbol_combined($fhai_currency_code);
+<?php
+if ( ! isset( $fhai_currency_code ) ) {
+	$fhai_currency_code = '';
+}
 
-foreach ($products as $fhai_product) :
+if ( ! isset( $fhai_currency_symbol ) ) {
+	$fhai_currency_symbol = fhai_currency_symbol_combined( $fhai_currency_code );
+}
 
-    // -------------------------------------------------
-    // Product styles
-    // -------------------------------------------------
-    $fhai_product_styles_group1 = $fhai_product_styles_group2 = $fhai_product_styles_group3 = '';
-    $fhai_product_styles_group4 = $fhai_product_styles_group5 = $fhai_product_styles_default = '';
-    $fhai_background_color = $fhai_product['pricing-background-color'] ?? '';
+foreach ( $products as $fhai_product ) :
 
-    if (!$fhai_background_color) {
-        switch ($fhai_product['name']) {
-            case in_array($fhai_product['name'], ['Helpdesk Freelancer','ServiceDesk Freelancer','Faveo Cloud HelpDesk','Support service','Customization','Faveo Upgrade','Install service']):
-                $fhai_product_styles_group1 = ' product-styles-group1'; break;
-            case in_array($fhai_product['name'], ['Helpdesk Startup (Perpetual)','Servicedesk Startup (Perpetual)','Helpdesk Startup (Recurring)','ServiceDesk Startup (Recurring)','Helpdesk Startup (Cloud)','ServiceDesk Startup (Cloud)']):
-                $fhai_product_styles_group2 = ' product-styles-group2'; break;
-            case in_array($fhai_product['name'], ['Helpdesk SME (Perpetual)','ServiceDesk SME (Perpetual)','Helpdesk SME (Recurring)','ServiceDesk SME (Recurring)','Helpdesk SME (Cloud)','ServiceDesk SME (Cloud)']):
-                $fhai_product_styles_group3 = ' product-styles-group3'; break;
-            case in_array($fhai_product['name'], ['Helpdesk Enterprise (Perpetual)','Helpdesk Enterprise (Recurring)','ServiceDesk Enterprise (Perpetual)','ServiceDesk Enterprise (Recurring)','Helpdesk Enterprise (Cloud)','ServiceDesk Enterprise (Cloud)']):
-                $fhai_product_styles_group4 = ' product-styles-group4'; break;
-            case in_array($fhai_product['name'], ['Helpdesk Enterprise Pro (Perpetual)','ServiceDesk Enterprise Pro (Perpetual)']):
-                $fhai_product_styles_group5 = ' product-styles-group5'; break;
-            default:
-                $fhai_product_styles_default = ' product-styles-default';
-        }
-    }
+	// -------------------------------------------------
+	// Product styles
+	// -------------------------------------------------
+	$fhai_product_styles_group1  = '';
+	$fhai_product_styles_group2  = '';
+	$fhai_product_styles_group3  = '';
+	$fhai_product_styles_group4  = '';
+	$fhai_product_styles_group5  = '';
+	$fhai_product_styles_default = '';
+	$fhai_background_color_raw   = $fhai_product['pricing-background-color'] ?? '';
+	$fhai_background_color       = fhai_sanitize_css_color( $fhai_background_color_raw );
 
-    // -------------------------------------------------
-    // Prices
-    // -------------------------------------------------
-    $fhai_monthly_price = floatval($fhai_product['add_price'] ?? 0);
-    $fhai_yearly_price  = floatval($fhai_product['add_price'] ?? 0);
-    $fhai_offer_price   = floatval($fhai_product['offer_price'] ?? 0);
-    $fhai_is_free_product = (!$all_status_one && $fhai_monthly_price <= 0);
-    $fhai_effective_price = $fhai_offer_price > 0
-        ? $fhai_monthly_price - ($fhai_monthly_price * ($fhai_offer_price / 100))
-        : $fhai_monthly_price;
+	if ( ! $fhai_background_color ) {
+		switch ( $fhai_product['name'] ) {
+			case in_array( $fhai_product['name'], array( 'Helpdesk Freelancer', 'ServiceDesk Freelancer', 'Faveo Cloud HelpDesk', 'Support service', 'Customization', 'Faveo Upgrade', 'Install service' ), true ):
+				$fhai_product_styles_group1 = ' product-styles-group1';
+				break;
+			case in_array( $fhai_product['name'], array( 'Helpdesk Startup (Perpetual)', 'Servicedesk Startup (Perpetual)', 'Helpdesk Startup (Recurring)', 'ServiceDesk Startup (Recurring)', 'Helpdesk Startup (Cloud)', 'ServiceDesk Startup (Cloud)' ), true ):
+				$fhai_product_styles_group2 = ' product-styles-group2';
+				break;
+			case in_array( $fhai_product['name'], array( 'Helpdesk SME (Perpetual)', 'ServiceDesk SME (Perpetual)', 'Helpdesk SME (Recurring)', 'ServiceDesk SME (Recurring)', 'Helpdesk SME (Cloud)', 'ServiceDesk SME (Cloud)' ), true ):
+				$fhai_product_styles_group3 = ' product-styles-group3';
+				break;
+			case in_array( $fhai_product['name'], array( 'Helpdesk Enterprise (Perpetual)', 'Helpdesk Enterprise (Recurring)', 'ServiceDesk Enterprise (Perpetual)', 'ServiceDesk Enterprise (Recurring)', 'Helpdesk Enterprise (Cloud)', 'ServiceDesk Enterprise (Cloud)' ), true ):
+				$fhai_product_styles_group4 = ' product-styles-group4';
+				break;
+			case in_array( $fhai_product['name'], array( 'Helpdesk Enterprise Pro (Perpetual)', 'ServiceDesk Enterprise Pro (Perpetual)' ), true ):
+				$fhai_product_styles_group5 = ' product-styles-group5';
+				break;
+			default:
+				$fhai_product_styles_default = ' product-styles-default';
+		}
+	}
 
-    // -------------------------------------------------
-    // Display price formatting
-    // -------------------------------------------------
-    if (class_exists('NumberFormatter') && $currency_code) {
-        $fhai_formatter = new NumberFormatter('en', NumberFormatter::CURRENCY);
-        $fhai_display_price  = $fhai_formatter->formatCurrency($fhai_effective_price, $currency_code);
-        $fhai_original_price = $fhai_formatter->formatCurrency($fhai_monthly_price, $currency_code);
-    } else {
-        if ($currency_code === 'INR') {
-            $fhai_display_price  = $currency_symbol . fhai_indian_number_format($fhai_effective_price);
-            $fhai_original_price = $currency_symbol . fhai_indian_number_format($fhai_monthly_price);
-        } else {
-            $fhai_display_price  = $currency_symbol . number_format($fhai_effective_price, 2);
-            $fhai_original_price = $currency_symbol . number_format($fhai_monthly_price, 2);
-        }
-    }
+	// -------------------------------------------------
+	// Prices
+	// -------------------------------------------------
+	$fhai_monthly_price   = floatval( $fhai_product['add_price'] ?? 0 );
+	$fhai_yearly_price    = floatval( $fhai_product['add_price'] ?? 0 );
+	$fhai_offer_price     = floatval( $fhai_product['offer_price'] ?? 0 );
+	$fhai_is_free_product = ( ! $all_status_one && $fhai_monthly_price <= 0 );
+	$fhai_effective_price = $fhai_offer_price > 0
+		? $fhai_monthly_price - ( $fhai_monthly_price * ( $fhai_offer_price / 100 ) )
+		: $fhai_monthly_price;
 
-    $fhai_product_key = isset($fhai_product['id'])
-        ? 'p-' . intval($fhai_product['id'])
-        : sanitize_title($fhai_product['name']);
-?>
+	// -------------------------------------------------
+	// Display price formatting
+	// -------------------------------------------------
+	if ( class_exists( 'NumberFormatter' ) && $fhai_currency_code ) {
+		$fhai_formatter      = new NumberFormatter( 'en', NumberFormatter::CURRENCY );
+		$fhai_display_price  = $fhai_formatter->formatCurrency( $fhai_effective_price, $fhai_currency_code );
+		$fhai_original_price = $fhai_formatter->formatCurrency( $fhai_monthly_price, $fhai_currency_code );
+	} elseif ( 'INR' === $fhai_currency_code ) {
+			$fhai_display_price  = $fhai_currency_symbol . fhai_indian_number_format( $fhai_effective_price );
+			$fhai_original_price = $fhai_currency_symbol . fhai_indian_number_format( $fhai_monthly_price );
+	} else {
+		$fhai_display_price  = $fhai_currency_symbol . number_format( $fhai_effective_price, 2 );
+		$fhai_original_price = $fhai_currency_symbol . number_format( $fhai_monthly_price, 2 );
+	}
 
-    <div class="product-container <?php
-        echo esc_attr(
-            $atts['style'] .
-            ($fhai_product['highlight'] == 1 ? ' highlighted-product-container' : '') .
-            $fhai_product_styles_group1 .
-            $fhai_product_styles_group2 .
-            $fhai_product_styles_group3 .
-            $fhai_product_styles_group4 .
-            $fhai_product_styles_group5 .
-            $fhai_product_styles_default
-        );
-    ?>"
-        data-product-key="<?php echo esc_attr($fhai_product_key); ?>"
-        data-group="<?php echo esc_attr($group_id); ?>"
-        data-days="<?php echo esc_attr($fhai_product['days']); ?>"
-        data-monthly="<?php echo esc_attr($fhai_monthly_price); ?>"
-        data-yearly="<?php echo esc_attr($fhai_yearly_price); ?>"
-        data-offer="<?php echo esc_attr($fhai_offer_price); ?>"
-        data-currency-code="<?php echo esc_attr($currency_code); ?>"
-        data-currency-symbol="<?php echo esc_attr($currency_symbol); ?>"
-        data-has-toggle="<?php echo $all_status_one ? '1' : '0'; ?>"
-        data-add-to-contact="<?php echo !empty($fhai_product['add_to_contact']) ? '1' : '0'; ?>"
-        data-is-free="<?php echo $fhai_is_free_product ? '1' : '0'; ?>"
-    >
+	$fhai_product_key = isset( $fhai_product['id'] )
+		? 'p-' . intval( $fhai_product['id'] )
+		: sanitize_title( $fhai_product['name'] );
+	?>
 
-        <div class="additional-container">
-            <div class="packagess" <?php if ($fhai_background_color) : ?>
-                style="background-color: <?php echo esc_attr($fhai_background_color); ?>;"
-            <?php endif; ?>>
+	<div class="product-container 
+	<?php
+		echo esc_attr(
+			$atts['style'] .
+			( 1 === (int) $fhai_product['highlight'] ? ' highlighted-product-container' : '' ) .
+			$fhai_product_styles_group1 .
+			$fhai_product_styles_group2 .
+			$fhai_product_styles_group3 .
+			$fhai_product_styles_group4 .
+			$fhai_product_styles_group5 .
+			$fhai_product_styles_default
+		);
+	?>
+									"
+		data-product-key="<?php echo esc_attr( $fhai_product_key ); ?>"
+		data-group="<?php echo esc_attr( $group_id ); ?>"
+		data-days="<?php echo esc_attr( $fhai_product['days'] ); ?>"
+		data-monthly="<?php echo esc_attr( $fhai_monthly_price ); ?>"
+		data-yearly="<?php echo esc_attr( $fhai_yearly_price ); ?>"
+		data-offer="<?php echo esc_attr( $fhai_offer_price ); ?>"
+		data-currency-code="<?php echo esc_attr( $fhai_currency_code ); ?>"
+		data-currency-symbol="<?php echo esc_attr( $fhai_currency_symbol ); ?>"
+		data-has-toggle="<?php echo $all_status_one ? '1' : '0'; ?>"
+		data-add-to-contact="<?php echo ! empty( $fhai_product['add_to_contact'] ) ? '1' : '0'; ?>"
+		data-is-free="<?php echo $fhai_is_free_product ? '1' : '0'; ?>"
+	>
 
-                <div class="product-pricing <?php echo $fhai_product['highlight']==1 ? 'highlighted-product' : ''; ?>">
+		<div class="additional-container">
+			<div class="packagess" <?php if ( $fhai_background_color ) : ?>
+				style="background-color: <?php echo esc_attr( $fhai_background_color ); ?>;"
+			<?php endif; ?>>
 
-                    <?php if ($fhai_product['highlight']==1) : ?>
-                        <div class="popular-ribbon text-light">Most Popular</div>
-                    <?php endif; ?>
+				<div class="product-pricing <?php echo 1 === (int) $fhai_product['highlight'] ? 'highlighted-product' : ''; ?>">
 
-                    <h1><?php echo esc_html($fhai_product['name']); ?></h1>
+					<?php if ( 1 === (int) $fhai_product['highlight'] ) : ?>
+						<div class="popular-ribbon text-light">Most Popular</div>
+					<?php endif; ?>
 
-                    <?php if (!empty($fhai_product['short_description'])) : ?>
-                        <div class="short-description"><?php echo wp_kses_post($fhai_product['short_description']); ?></div>
-                    <?php endif; ?>
+					<h1><?php echo esc_html( $fhai_product['name'] ); ?></h1>
 
-                    <?php if (!empty($fhai_product['add_to_contact'])) : ?>
-                        <h2 class="custom-pricing">
-                            Custom Pricing
-                        </h2>
-                        <p class="price-description"><?php echo wp_kses_post($fhai_product['price_description']); ?></p>
-                        <a href="<?php echo esc_url(get_option('fhai_custom_sales_url', 'https://www.example.com/')); ?>" class="purchase-btn">Contact Sales</a>
-                    <?php else : ?>
-                       <h2 
-    data-monthly-price="<?php echo esc_attr($fhai_monthly_price); ?>"
-    data-yearly-price="<?php echo esc_attr($fhai_yearly_price); ?>"
-    data-currency-symbol="<?php echo esc_attr($currency_symbol); ?>"
-    data-currency-code="<?php echo esc_attr($currency_code); ?>"
+					<?php if ( ! empty( $fhai_product['short_description'] ) ) : ?>
+						<div class="short-description"><?php echo wp_kses_post( $fhai_product['short_description'] ); ?></div>
+					<?php endif; ?>
+
+					<?php if ( ! empty( $fhai_product['add_to_contact'] ) ) : ?>
+						<h2 class="custom-pricing">
+							Custom Pricing
+						</h2>
+						<p class="price-description"><?php echo wp_kses_post( $fhai_product['price_description'] ); ?></p>
+						<a href="<?php echo esc_url( get_option( 'fhai_custom_sales_url', 'https://www.example.com/' ) ); ?>" class="purchase-btn">Contact Sales</a>
+					<?php else : ?>
+						<h2 
+	data-monthly-price="<?php echo esc_attr( $fhai_monthly_price ); ?>"
+	data-yearly-price="<?php echo esc_attr( $fhai_yearly_price ); ?>"
+	data-currency-symbol="<?php echo esc_attr( $fhai_currency_symbol ); ?>"
+	data-currency-code="<?php echo esc_attr( $fhai_currency_code ); ?>"
 >
-    <?php echo $fhai_is_free_product ? 'Free' : esc_html($fhai_display_price); ?>
+						<?php echo $fhai_is_free_product ? 'Free' : esc_html( $fhai_display_price ); ?>
 </h2>
 
 
-                        <?php if ($fhai_offer_price > 0) : ?>
-                            <p class="original-price"><s><?php echo esc_html($fhai_original_price); ?></s></p>
-                        <?php endif; ?>
+						<?php if ( $fhai_offer_price > 0 ) : ?>
+							<p class="original-price"><s><?php echo esc_html( $fhai_original_price ); ?></s></p>
+						<?php endif; ?>
 
-                        <p class="price-description"><?php echo wp_kses_post($fhai_product['price_description']); ?></p>
-                        <a href="<?php echo esc_url($fhai_product['shoping_cart_link']); ?>" class="purchase-btn">Buy Now</a>
-                    <?php endif; ?>
+						<p class="price-description"><?php echo wp_kses_post( $fhai_product['price_description'] ); ?></p>
+						<a href="<?php echo esc_url( $fhai_product['shoping_cart_link'] ); ?>" class="purchase-btn">Buy Now</a>
+					<?php endif; ?>
 
-                </div>
+				</div>
 
-                <div class="description">
-                    <?php
-                    $fhai_description_with_tooltips = preg_replace_callback(
-                        '/<li([^>]*)>(.*?)<\/li>/i',
-                        function ($matches) {
-                            $tooltip = wp_strip_all_tags($matches[2]);
-                            return '<li data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="top" data-bs-content="' .
-                                esc_attr($tooltip) . '">' . $matches[2] . '</li>';
-                        },
-                        $fhai_product['description']
-                    );
-                    echo wp_kses_post($fhai_description_with_tooltips);
-                    ?>
-                </div>
+				<div class="description">
+					<?php
+					$fhai_description_with_tooltips = preg_replace_callback(
+						'/<li([^>]*)>(.*?)<\/li>/i',
+						function ( $matches ) {
+							$tooltip = wp_strip_all_tags( $matches[2] );
+							return '<li data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-placement="top" data-bs-content="' .
+								esc_attr( $tooltip ) . '">' . $matches[2] . '</li>';
+						},
+						$fhai_product['description']
+					);
+					echo wp_kses_post( $fhai_description_with_tooltips );
+					?>
+				</div>
 
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+	</div>
 
 <?php endforeach; ?>
 </div>
